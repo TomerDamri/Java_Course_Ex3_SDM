3b23e080c47ae20ed585702bed87a2ff69f43856
TomerDamri <tomerdamri800@gmail.com>
2020-09-26 22:21:53 +0300

    Fixed final summary representation

--- data-model/src/model/response/FinalSummaryForOrder.java
+++ data-model/src/model/response/FinalSummaryForOrder.java
@@ -4,10 +4,18 @@ import java.util.Iterator;
 import java.util.List;
 
 public class FinalSummaryForOrder {
-
+    private final double totalItemsPrice;
+    private final double totalDeliveryPrice;
+    private final double totalPrice;
     List<StoreSummaryForOrder> orderIncludedStoresDetails;
 
-    public FinalSummaryForOrder (List<StoreSummaryForOrder> orderIncludedStoresDetails) {
+    public FinalSummaryForOrder (double totalItemsPrice,
+                                 double totalDeliveryPrice,
+                                 double totalPrice,
+                                 List<StoreSummaryForOrder> orderIncludedStoresDetails) {
+        this.totalItemsPrice = totalItemsPrice;
+        this.totalDeliveryPrice = totalDeliveryPrice;
+        this.totalPrice = totalPrice;
         this.orderIncludedStoresDetails = orderIncludedStoresDetails;
     }
 
@@ -26,7 +34,13 @@ public class FinalSummaryForOrder {
                 builder.append(",\n");
             }
         }
-        builder.append("\n");
+        builder.append("\n\n")
+               .append("totalItemsPrice= ")
+               .append(totalItemsPrice)
+               .append(",\n totalDeliveryPrice= ")
+               .append(totalDeliveryPrice)
+               .append(",\n totalPrice= ")
+               .append(totalPrice);
 
         return builder.toString();
     }
